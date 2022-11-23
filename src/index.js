ddccb6a5c656e8f8e63b6a6c49e3316506fe93a3
buhaiov <a.buhaiov@urancompany.com>
2022-11-23 10:47:34 +0200

    add prettier , refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Editor from '@models/Editor'
 import './styles/scss.scss'
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const editor = new Editor
+const editor = new Editor()
--- src/utils/intersect.js
+++ src/utils/intersect.js
@@ -1,7 +1,7 @@
-const intersect = function(arr1, arr2) {
-    return arr1.filter(function(n) {
-        return arr2.indexOf(n) !== -1;
-    });
-};
+const intersect = function (arr1, arr2) {
+	return arr1.filter(function (n) {
+		return arr2.indexOf(n) !== -1
+	})
+}
 
 module.exports = intersect
